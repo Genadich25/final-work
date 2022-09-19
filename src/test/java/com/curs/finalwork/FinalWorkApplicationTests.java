5001ce5dc8418d03597b1dd8feb779a34fba29e9
pin20 <pin200048@gmail.com>
2022-09-19 17:09:11 +0400

    Initial commit

---
+++ src/test/java/com/curs/finalwork/FinalWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curs.finalwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
